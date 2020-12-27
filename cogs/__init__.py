06fd8c40c385b54f0da8254d780fbdcc00ea09ed
cyrus01337 <cyrus21337@gmail.com>
2020-12-27 04:06:00 +0000

    Implement cog inheritance subsystem

---
+++ cogs/__init__.py
@@ -0,0 +1,5 @@
+from . import __template as template
+from .error_handler import ErrorHandler
+from .information import HelpCommand, Information
+from .owner import Owner
+from .testing import Testing
