b4411093eac569239f628f52a3a0be700ae83698
cyrus01337 <cyrus21337@gmail.com>
2021-01-02 06:35:25 +0000

    Implement custom typehints (Flake8: E501)

---
+++ typings.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+import discord
+from discord.ext import commands
+
+
+class Literals:
+    destination = (commands.Context, discord.TextChannel)
+
+
+Destination = Union[Literals.desination]
+literals = Literals
