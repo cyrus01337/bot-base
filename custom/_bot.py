1111e18905463c9b2460807889515706731f226f
cyrus01337 <cyrus21337@gmail.com>
2021-01-18 13:02:20 +0000

    Add/Replace custom typings, tweak default behaviour, add onto command processing

--- custom/_bot.py
+++ custom/_bot.py
@@ -1,21 +1,17 @@
 import copy
-import inspect
 import os
 import sys
-import traceback
-from asyncio import Event
-from collections.abc import Iterable
-from json import dumps
-from typing import Dict
+from asyncio import Event, Task
+from typing import Dict, List, Tuple
 
 import aiohttp
 import discord
 from discord.ext import commands
 
-# from base import cogs
 from base import errors
 from base import utils
 from base.resources import PREFIXES
+from base.typings import overwritable
 
 
 class Bot(commands.Bot):
@@ -33,79 +29,83 @@ class Bot(commands.Bot):
             "activity",
             discord.Activity(
                 type=discord.ActivityType.listening,
-                name="pings"
+                name="@Administrator prefix"
             )
         )
 
+        self._on_ready_tasks: List[Task] = []
         self._display = Event()
-        self.session = aiohttp.ClientSession()
+        self.home_id: int = kwargs.pop("home_id", None)
+        self.error_log_id: int = kwargs.pop("error_log_id", None)
+        self.mentions: Tuple[str] = None
         self.reactions: Dict[bool, str] = kwargs.pop("reactions", {
             True: "\U0001f44e",
             False: "\U0001f44d"
         })
+        self.session = aiohttp.ClientSession()
         self.permissions: discord.Permissions = kwargs.pop(
             "permissions",
             discord.Permissions()
         )
 
         super().__init__(*args, **kwargs)
-        self._wrap_coroutines(self.__ainit__, self.display)
+        for coroutine in (self.__ainit__, self.display):
+            task = self.loop.create_task(coroutine())
+            task.add_done_callback(self._startup_error)
 
-    def __init_subclass__(cls):
-        ainit = getattr(cls, "__ainit__", None)
+            self._on_ready_tasks.append(task)
 
-        if ainit and inspect.iscoroutinefunction(ainit):
-            cls.__ainit__ = utils.when_ready(ainit)
-
-    # overwritable
-    @utils.when_ready
+    @overwritable
+    @utils.when_ready()
     async def __ainit__(self):
         pass
 
+    @overwritable
+    @utils.when_ready()
+    async def display(self):
+        await self.default_display()
+
     @property
+    @utils.has_intents(guilds=True)
     def home(self):
-        return self.get_guild(464446709146320897)
+        return self.get_guild(self.home_id)
 
     @property
+    @utils.has_intents(guilds=True)
     def error_log(self):
-        return self.home.get_channel(778758649938051073)
-
-    def _formatted(self, iterable: Iterable, **kwargs):
-        # for usage with Jishaku
-        kwargs["indent"] = kwargs.get("indent", 4)
-
-        if isinstance(iterable, map):
-            iterable = tuple(iterable)
-        return (f"```py\n"
-                f"{dumps(iterable, **kwargs)}\n"
-                f"```")
+        return self.home.get_channel(self.error_log_id)
 
     def _startup_error(self, future):
+        if future.cancelled():
+            return
         error = future.exception()
 
         if error is not None:
             self.dispatch("startup_error", error)
-
-    def _wrap_coroutines(self, *coroutines):
-        for coroutine in coroutines:
-            task = self.loop.create_task(coroutine())
-            task.add_done_callback(self._startup_error)
 
     def _strip_prefix(self, content, prefixes):
         match_found = None
 
         for prefix in prefixes:
             if content.startswith(prefix):
-                # in the instance of having multiple prefixes similar to
-                # each other, this grabs both a prefix serving as the
-                # initial match and the longest possible prefix able to
-                # be matched
+                # in the instance of having multiple, simiar prefixes,
+                # this grabs both a prefix serving as the initial match
+                # and the longest possible prefix able to be matched
                 if not match_found or len(prefix) > len(match_found):
                     match_found = prefix
 
         if match_found:
             return content[len(match_found):]
         raise ValueError(f'prefix cannot be stripped from "{content}"')
+
+    def _get_cogs(self, path: str):
+        ret = ["jishaku"]
+
+        for file in os.listdir(path):
+            if file.startswith("__") is False and file.endswith(".py"):
+                resolved_path = utils.resolve_path(os.path.join(path, file))
+                ret.append(resolved_path)
+        return ret
 
     async def _autocomplete_command(self, message):
         prefixes = tuple(self.command_prefix(self, message))
@@ -146,16 +146,7 @@ class Bot(commands.Bot):
                                 return command
         return None
 
-    def get_cogs(self, path: str):
-        ret = ["jishaku"]
-
-        for file in os.listdir(path):
-            if file.startswith("__") is False and file.endswith(".py"):
-                resolved_path = utils.resolve_path(os.path.join(path, file))
-                ret.append(resolved_path)
-        return ret
-
-    def load_extensions(self, path: str = "base/cogs", exclude: Iterable = ()):
+    def load_extensions(self, path: str = "base/cogs", silent: bool = False):
         paths = []
 
         if path != "base/cogs":
@@ -163,9 +154,7 @@ class Bot(commands.Bot):
         paths.append(path)
 
         for cog_path in paths:
-            for cog in self.get_cogs(cog_path):
-                if cog in exclude:
-                    continue
+            for cog in self._get_cogs(cog_path):
                 method = "[ ] Loaded"
 
                 try:
@@ -180,45 +169,30 @@ class Bot(commands.Bot):
                     if isinstance(error, commands.ExtensionFailed):
                         error = error.original
                     self.dispatch("startup_error", error)
-                print(f"{method} cog: {cog}")
+
+                if not silent:
+                    print(f"{method} cog: {cog}")
+
+    def trigger_display(self):
+        if not self._display.is_set():
+            self._display.set()
+
+    async def default_display(self):
+        utils.clear_screen()
+        print(self.user.name, end="\n\n")
+
+        self.trigger_display()
 
     async def wait_for_display(self):
         if not self._display.is_set():
             await self._display.wait()
 
-    @commands.Cog.listener()
-    async def on_startup_error(self, error):
-        await self.wait_for_display()
-        formatted = traceback.format_exception(
-            type(error),
-            error,
-            error.__traceback__
-        )
-        joined = ("").join(formatted)
-        print(joined, file=sys.stderr)
-
-    # overwritable
-    def handle_display(self):
-        if not self._display.is_set():
-            self._display.set()
-
-    # overwritable
-    @utils.when_ready
-    async def display(self):
-        utils.clear_screen()
-        print(self.user.name, end="\n\n")
-
-        self.handle_display()
-
-    # overwritten methods
-    async def on_message(self, message):
-        autocompleted = await self._autocomplete_command(message)
-
-        if not autocompleted:
-            await self.process_commands(message)
-
     def load_extension(self, name):
-        return super().load_extension(name)
+        # module = importlib.import_module(name)
+
+        # print(module, (", ").join(dir(module)))
+
+        super().load_extension(name)
 
     def run(self, token=None, **kwargs):
         path = "./TOKEN"
@@ -232,6 +206,27 @@ class Bot(commands.Bot):
                 raise errors.TokenFileNotFound(f'{full}" not found')
         super().run(token, **kwargs)
 
+    async def on_message(self, message):
+        autocompleted = await self._autocomplete_command(message)
+
+        if self.mentions and message.content in self.mentions:
+            alt_message = copy(message)
+            alt_message.content = f"{message.content} help"
+            ctx = await self.get_context(alt_message)
+
+            await self.invoke(ctx)
+        elif not autocompleted:
+            await self.process_commands(message)
+
+    @commands.Cog.listener()
+    async def on_startup_error(self, error):
+        await self.wait_for_display()
+        message = utils.format_exception(error)
+        print(message, file=sys.stderr)
+
     async def close(self):
+        for task in self._on_ready_tasks:
+            task.cancel()
+
         await self.session.close()
         await super().close()
