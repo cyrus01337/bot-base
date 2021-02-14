5adde03e34dc777f45def43cb09e4a7a7bbe69d3
cyrus01337 <cyrus21337@gmail.com>
2021-02-14 15:11:37 +0000

    Add onto command processing

--- custom/context.py
+++ custom/context.py
@@ -1,27 +1,29 @@
+import discord
 from discord.ext import commands
+
+from base import utils
 
 
 # https://github.com/platform-discord/travis-bott/blob/master/utils/customcontext.py#L33-L79
 class Context(commands.Context):
     async def send(self, *args, **kwargs):
-        is_not_jishaku = (self.command.cog and
-                          self.command.cog.qualified_name != "Jishaku")
-
-        if self.bot.shutdown and not is_not_jishaku:
+        if self.bot.shutdown and not await self.bot.shutdown_check(ctx=self):
             return print("[S] Bot has been locally shutdown")
         is_owner = await self.bot.is_owner(self.author)
         message = self.message
 
-        if is_owner:
-            cached = self.bot._get_edit_cached_message(self.message.id)
+        if not is_owner:
+            return await super().send(*args, **kwargs)
+        cached = self.bot._get_edit_cached_message(self.message.id)
 
-            if cached:
-                if args:
-                    kwargs["content"] = args[0]
-                await cached.clear_reactions()
+        if cached:
+            try:
+                await utils.clear_reactions(cached)
                 await cached.edit(**kwargs)
                 self.bot._edit_cache[self.message.id] = cached
-            else:
-                message = await super().send(*args, **kwargs)
-                self.bot._edit_cache[self.message.id] = message
+            except discord.NotFound:
+                del self.bot._edit_cache[self.message.id]
+        else:
+            message = await super().send(*args, **kwargs)
+            self.bot._edit_cache[self.message.id] = message
         return message
