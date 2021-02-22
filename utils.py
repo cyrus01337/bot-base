f7b9ead652ee750c0971983401bf87696246aa28
cyrus01337 <cyrus21337@gmail.com>
2021-02-22 08:11:05 +0000

    Add things that helped simplify tedious processes

--- utils.py
+++ utils.py
@@ -1,10 +1,13 @@
+import asyncio
 import contextlib
+import functools
 import json
 import re
 import os
 import traceback
-from functools import wraps
-from typing import Callable, Iterable, Union
+from collections.abc import Container
+from pathlib import Path
+from typing import Any, Callable, Iterable, Optional, Union
 
 import discord
 
@@ -15,19 +18,6 @@ MULTIPLE_SPACES = re.compile(r" +")
 
 def clear_screen():
     return os.system("cls" if os.name == "nt" else "clear")
-
-
-def when_ready():
-    def inner(coroutine):
-        @wraps(coroutine)
-        async def predicate(*args, **kwargs):
-            self = args[0]
-            bot = getattr(self, "bot", self)
-
-            await bot.wait_until_ready()
-            return await coroutine(*args, **kwargs)
-        return predicate
-    return inner
 
 
 def strip_multi_space(string: str):
@@ -49,6 +39,51 @@ def format_exception(error: Exception):
         error.__traceback__
     )
     return ("").join(blueprint)
+
+
+def dotted(path: Union[Path, str]):
+    path = Path(path).with_suffix("")
+    parts = path.parts
+
+    if path.drive:
+        parts = parts[1:]
+    return (".").join(parts)
+
+
+def prioritise(iterable, *, reverse: bool = False, **attrs):
+    good = 1
+    bad = 2
+
+    if reverse:
+        good = 2
+        bad = 1
+
+    def key(obj):
+        for key, value in attrs.items():
+            try:
+                attr = getattr(obj, key)
+            except AttributeError:
+                return bad
+            else:
+                is_container = isinstance(value, Container)
+                if (is_container and attr in value) or attr == value:
+                    continue
+                return bad
+        return good
+    return sorted(iterable, key=key)
+
+
+def when_ready():
+    def inner(coroutine):
+        @functools.wraps(coroutine)
+        async def predicate(*args, **kwargs):
+            self = args[0]
+            bot = getattr(self, "bot", self)
+
+            await bot.wait_until_ready()
+            return await coroutine(*args, **kwargs)
+        return predicate
+    return inner
 
 
 # https://github.com/Rapptz/discord.py/blob/master/discord/ext/commands/core.py#L1784-L1808
@@ -82,3 +117,49 @@ async def clear_reactions(message: discord.Message):
         with contextlib.suppress(discord.HTTPException):
             for reaction in message.reactions:
                 await message.remove_reaction(reaction, message.guild.me)
+
+
+async def repeat_get(self,
+                     obj: Any,
+                     key: str,
+                     *, not_value: Optional[Any] = None,
+                     use_is: bool = False,
+                     wait: Union[int, float] = 1,
+                     timeout: Union[int, float] = -1):
+    waited = 0
+
+    while True:
+        try:
+            value = getattr(obj, key)
+        except AttributeError as error:
+            waited += 1
+
+            await asyncio.sleep(wait)
+
+            if timeout != -1 and waited >= timeout:
+                raise error
+        else:
+            if use_is:
+                valid = value is not not_value
+            else:
+                valid = value != not_value
+
+            if not valid:
+                continue
+            return value
+
+
+class Flags:
+    def __init__(self, *flags: str, **mapping: bool):
+        for flag in map(str.lower, flags):
+            os.environ[flag] = "True"
+
+        for flag, value in mapping.items():
+            flag = flag.lower()
+
+            if flag != "scope_prefix":
+                flag = f"jishaku_{flag}"
+            os.environ[flag] = str(value)
+
+
+prioritize = prioritise
