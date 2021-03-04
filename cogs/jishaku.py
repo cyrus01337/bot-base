67272b6c53d6c62c789fefc535c30593a4c328f9
cyrus01337 <cyrus21337@gmail.com>
2021-03-04 02:56:59 +0000

    Add cleanup command, enable Py <3.9 compatibility

--- cogs/jishaku.py
+++ cogs/jishaku.py
@@ -10,12 +10,24 @@ MYSTBIN = re.compile(r"(https://mystb\.in/([A-Z][a-z]+){3}(\.[A-Z]?[a-z]+)?)")
 class Jishaku(*STANDARD_FEATURES, *OPTIONAL_FEATURES):
     @Feature.Command(parent="jsk", name="py", aliases=["python"])
     async def jsk_python(self, ctx, *, argument):
-        if match_found := MYSTBIN.match(argument):
+        match_found = MYSTBIN.match(argument)
+
+        if match_found:
             url = match_found.group()
             response = await self.bot.mystbin.get(url)
             argument = str(response)
         argument = codeblock_converter(argument)
         await PythonFeature.jsk_python.callback(self, ctx, argument=argument)
+
+    @Feature.Command(parent="jsk", name="cleanup")
+    async def jsk_cleanup(self, ctx, iterations: int = 1):
+        limit = 100 * iterations
+
+        def check(message):
+            return message.author == ctx.bot.user
+
+        deleted = await ctx.channel.purge(limit=limit, check=check, bulk=False)
+        await ctx.send(f"Deleted {len(deleted):,} message(s)!", delete_after=5)
 
 
 def setup(bot):
