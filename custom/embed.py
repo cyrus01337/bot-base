96cddbc40cc1754aeb4e4b0b43b9778c023a41b0
cyrus01337 <cyrus21337@gmail.com>
2021-04-26 00:11:25 +0100

    Fix empty field values

--- custom/embed.py
+++ custom/embed.py
@@ -4,8 +4,10 @@ import discord
 
 
 class Field(object):
-    def __init__(self, name: str = "\u200b",
-                 value: str = "\u200b", inline: bool = False):
+    def __init__(self,
+                 name: str = "\u200b",
+                 value: str = "\u200b",
+                 inline: bool = False):
         self._dict = dict(name=name, value=value, inline=inline)
         self.name = name
         self.value = value
@@ -17,27 +19,26 @@ class Field(object):
         return self._dict[key]
 
     def __repr__(self):
-        return (f"<Field name={repr(self.name)} "
-                f"value={repr(self.value)} "
-                f"inline={repr(self.inline)}>")
+        return (
+            f"<Field name={repr(self.name)} value={repr(self.value)} "
+            f"inline={repr(self.inline)}>"
+        )
 
 
 class Embed(discord.Embed):
     def __init__(self, *args, **kwargs):
         fields = kwargs.pop("fields", [])
         kwargs.setdefault("color", kwargs.pop("colour", 0x7289DA))
-        kwargs.setdefault("description", kwargs.pop("desc", None))
 
+        if kwargs.get("desc"):
+            kwargs["description"] = kwargs.pop("desc")
         super().__init__(*args, **kwargs)
-        self._fields = []
 
         if isinstance(fields, Field):
-            self._fields.append({**fields})
-        else:
-            for field in fields:
-                self._fields.append({**field})
+            fields = [fields]
+        self.add_fields(fields)
 
-    def add_fields(self, fields: Iterable):
+    def add_fields(self, fields: Iterable[Field]):
         for field in fields:
-            self._fields.append({**fields})
+            self.add_field(**field._dict)
         return self
